d2f8085679164bbbb92fdc4e34881fa730d68f68
Bryan Lee <Bryan@Bryans-MacBook-Air.local>
2023-10-09 08:07:37 -0500

    evalPRN()

--- stack.py
+++ stack.py
@@ -51,4 +51,20 @@ class MinStack:
 # print(stackObj.getMin())
 
 
+# 3. Evaluate Reverse Polish Notation
+def evalRPN(tokens):
 
+    p = []
+    for i in tokens:
+        if i == "+" or i == "-" or i == "*" or i == "/":
+            a = p.pop()
+            b = p.pop()
+            p.append(int(eval(str(b)+i+str(a))))
+        else: 
+            p.append(i)
+    
+    return int(p[0])
+
+tokens = ["2","1","+","3","*"]
+print(evalRPN(tokens))
+        
